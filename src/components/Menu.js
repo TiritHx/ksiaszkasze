fc9446ba9aeabd355ddcb87563984b700a59ebfa
NivienPalka <91349142+NivienPalka@users.noreply.github.com>
2023-09-13 09:34:37 +0200

    Add files via upload
    boom

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Menu.css";
 
 function Menu() {
   return (
     <div>
-
+      <div className="menu">
+        <div className="menu-item">Profil</div>
+        <div className="menu-item">Strona główna</div>
+        <div className="menu-item">Zaloguj</div>
+        <div className="menu-item">Stwórz konto</div>
+        <div className="menu-item">
+          <input className="search" placeholder="Wyszukaj"/>
+          <button className="search-button">Szukaj</button>
+          </div>
+      </div>
     </div>
   );
 }

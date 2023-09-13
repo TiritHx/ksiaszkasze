bc13743f2e9907d66f5406c1632425f09aadb66c
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-09-13 09:51:56 +0200

    klon broke 😿🤮
    I repear 😎🤑

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Menu from './components/Menu';
 import Tile from './components/Tile';
 
 function App() {
-  const DATA = [{title: "tytuł", src: "./mhm", rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9}];
+  const DATA = [{title: "tytuł", src: null, rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9},{title: "tytuł", src: null, rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9},{title: "tytuł", src: null, rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9},{title: "tytuł", src: null, rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9},{title: "tytuł", src: null, rating: 5.8},{title: "ksiąszka", src: null, rating: 9.9},];
   return (
     <div>
       <Menu />
-      {DATA.map(x => <Tile title={x.title} src={x.src} rating={x.rating} />)}
+      <div className='container'>
+        {DATA.map(x => <Tile title={x.title} src={x.src} rating={x.rating} />)}
+      </div>
     </div>
   );
 }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,12 @@
 import React from "react";
+import noimage from "../images/noimage.jpg";
 
 function Tile(props) {
+  const src = props.src === null ? noimage : props.src;
   return (
-    <div className="border">
+    <div className="border tile">
       <p>{props.title}</p>
-      <img src={props.src} />
+      <img src={src} alt="placeholder" />
       <p>{props.rating}</p>
     </div>
   );
